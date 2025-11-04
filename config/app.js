f2e677f479c30ec2d68a9ec7eb79d8f46b708049
minhit6505 <minhit6505@gmail.com>
2025-11-04 22:10:53 +0700

    commited

--- config/app.js
+++ config/app.js
@@ -5,6 +5,10 @@ module.exports = {
     urlPath: "/assets",
     folderPath: `${__dirname}/../src/public`,
   },
+  cors: {
+    origin: true,
+    credentials: true,
+  },
   jwtAccessKey: process.env.JWT_ACCESS_KEY || "vietpro_access_key",
   jwtRefreshKey: process.env.JWT_REFRESH_KEY || "vietpro_refresh_key",
 };
--- src/apps/app.js
+++ src/apps/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("config");
+const cors = require("cors");
 const redisClient = require("../common/init.redis");
 const app = express();
 app.use(bodyParser.json());
@@ -9,6 +10,12 @@ app.use(cookieParser());
 app.use(
   config.get("app.static.urlPath"),
   express.static(config.get("app.static.folderPath"))
+);
+app.use(
+  cors({
+    origin: config.get("app.cors.origin"), // Tự động phản hồi đúng origin request đến
+    credentials: config.get("app.cors.credentials"), // Cho phép gửi cookie, auth headers
+  })
 );
 
 app.use(config.get("app.prefixApiVersion"), require("../routers/web"));
