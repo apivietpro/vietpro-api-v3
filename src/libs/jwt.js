11c7378b003aa6bce2d716e8016bc628bc7020c0
minhit6505 <minhit6505@gmail.com>
2026-01-20 13:13:09 +0700

    commited

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -7,7 +7,7 @@ exports.generateAccessToken = async (payload) =>
       email: payload.email,
     },
     config.get("app.jwtAccessKey"),
-    { expiresIn: "1d" }
+    { expiresIn: "1000m" }
   );
 exports.generateRefreshToken = async (payload) =>
   await jwt.sign(
